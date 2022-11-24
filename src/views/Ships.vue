58f4a01163f86f32f3e3d23673bcd0ad1186d152
SkippTekk <skipptekk@gmail.com>
2022-11-23 22:59:11 -0700

    Setup for Axios testing

--- src/views/Ships.vue
+++ src/views/Ships.vue
@@ -8,11 +8,27 @@
 
    <body>
     <Biscuits />
-<!-- script goes here from mike-->
-
-
-<!-- Footsie for the footer-->
-
+    <Announcement />
+      <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <Navbar />
+      </nav>
+        <Announcment />
+      <section class='container-fluid'>
+        <div class='row'>
+          <div class='col order-1'>
+            <Leftbar />
+          </div>
+          <div class='col order-2'>
+            <Middlebar />
+          </div>
+          <div class='col order-3'>
+            <Rightbar />
+          </div>
+        </div>
+          <div>
+            <Footer />
+          </div>
+        </section>
  </body>
      </html>
 </template>
@@ -20,13 +36,24 @@
 // @ is an alias to /src
 import Biscuits from '@/templates/Biscuits.vue'
 import Title from '@/templates/Title.vue'
+import Announcement from '@/content/Announcement.vue'
 import Navbar from '@/Ships/Navbar.vue'
+import Footer from '@/content/Footer.vue'
+import Leftbar from '@/Ships/Leftbar.vue'
+import Middlebar from '@/Ships/Middlebar.vue'
+import Rightbar from '@/Ships/Rightbar.vue'
+
 export default {
   name: 'Ships',
   components: {
     Title,
     Biscuits,
-    Navbar
-  }
+    Navbar,
+    Footer,
+    Leftbar,
+    Middlebar,
+    Rightbar,
+    Announcement
+}
 }
 </script>
